73426c5ebe883770e7e275cad8a8f669f142d8a3
Nathan Richardson <nathanjrichardson3@gmail.com>
2020-02-01 20:54:02 -0600

    routes.js server is now connected to mysql db and making successful queries. The results are being posted to the /posts page as json. Next: add ability to save posts

--- src/util/routes.js
+++ src/util/routes.js
@@ -1,23 +1,52 @@
 const express = require('express');
-const cors = require('cors');
+const bodyParser = require('body-parser');
 const mysql = require('mysql');
+const cors = require('cors');
 
-
-const connection = mysql.createPool({
-	host	: 'localhost',
-	user	: 'root',
-	password: '135Howl84$$$',
-	database: 'test'
-});
-
+// STARTING THE APP \\
 const app = express()
 
+
+const SELECT_ALL_POSTS_QUERY = 'SELECT * FROM test_table';
+
+//CREATE AND CONFIGURE CONNECTION TO DB \\
+const connection = mysql.createConnection({
+	host	: 'localhost',
+	port 	: '3306',
+	user	: 'root',
+	password: '135Howl84$$$',
+	database: 'test',
+	connectionlimit: 10
+});
+
+//CONNECT TO DB \\
+connection.connect(err => {
+	if (err) { return err; };
+});
+
+app.use(cors());
+
+// MAKE REQUEST TO URL'S INDEX-PAGE AND POST TEXT TO WEBPAGE \\
 app.get('/', (req, res) => {
 	res.send('hello from the products server');
 })
 
+app.get('/posts', (req, res) => {
+	connection.query('SELECT * FROM test_table', (err, results) => {
+		if(err) {
+			return res.send('its the query');
+		} else {
+			return res.json({
+				data: results
+			})
+		}
+	});
+});
 
-app.use(cors());
+
+
+
+
 
 app.listen(8000, () => {
 	console.log('server running on 8000')
